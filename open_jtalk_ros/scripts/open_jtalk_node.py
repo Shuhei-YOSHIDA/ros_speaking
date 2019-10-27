e7d7bf35932032ef4735d5cd7d374bef5ec587b6
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2019-10-27 21:07:30 +0900

    added service SetVoiceData

--- open_jtalk_ros/scripts/open_jtalk_node.py
+++ open_jtalk_ros/scripts/open_jtalk_node.py
@@ -8,6 +8,7 @@ import rospy
 import rospkg
 from std_msgs.msg import String
 from open_jtalk_ros.msg import Words
+from open_jtalk_ros.srv import *
 import subprocess
 
 def openjtalk(message, voicefilepath, speak_speed=1.0):
@@ -33,25 +34,36 @@ def openjtalk(message, voicefilepath, speak_speed=1.0):
 
     return;
 
+
 class OpenJtalkROS(object):
     def __init__(self, ):
         rospy.init_node('open_jtalk_ros_node', anonymous=False)
         rospy.loginfo("start speaking")
         self.voicefilepath = rospkg.RosPack().get_path("open_jtalk_ros")+'/hts-voice/htsvoice-tohoku-f01/tohoku-f01-neutral.htsvoice'
+        self.voice_speed = 1.0
 
     def __speaker(self, data):
         rospy.loginfo("openjtalk says: %s", data.data)
 
-        openjtalk(data.data, self.voicefilepath)
+        openjtalk(data.data, self.voicefilepath, self.voice_speed)
 
     def __wordsSpeaker(self, data):
         rospy.loginfo("openjtalk says: %s", data.words)
 
         openjtalk(data.words, data.voice_data_path, data.voice_speed)
 
+    def __setVoiceData(self, req):
+        self.voicefilepath = req.voice_data_path # check validity of path
+        self.voice_speed = req.voice_speed
+
+        return SetVoiceDataResponse()
+
     def run(self):
         rospy.Subscriber("chatter", String, self.__speaker, queue_size=1)
         rospy.Subscriber("words", Words, self.__wordsSpeaker, queue_size=1)
+
+        rospy.Service('set_voice_data', SetVoiceData, self.__setVoiceData)
+
         rospy.spin()
 
 if __name__ == '__main__':
